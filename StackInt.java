5e7a2238cd99191ac797ebfb9140d886ff91ebbd
AndrewK <43419144+AndrewKuekam17@users.noreply.github.com>
2020-10-28 12:17:41 -0400

    Create StackInt.java

---
+++ StackInt.java
@@ -0,0 +1,37 @@
+import java.io;
+import java.util.Scanner;
+/**
+ * Class MyIntStack
+ */
+/**
+*
+* @author  Andrew Kuekam
+* @version 1.0
+* @since   2020-10-23 
+*/
+public class StackIn {
+  
+    //private properties
+    private stack (< Interger > stack) {
+      
+      // constuctor
+      public stackInt () {
+        stat = new stack ( < Interger > );
+      }
+      
+      public void pushInt ( Interger ) {
+        stack.push;
+      }
+      
+      public void PopInt ( Interger ) {
+        stack.Pop;
+      }
+      
+      public void PeekInt ( Interger ) {
+        stack.Peek;
+      }
+      public void ClearInt ( Interger ) {
+        stack.Clear;
+      }
+    }
+  }
