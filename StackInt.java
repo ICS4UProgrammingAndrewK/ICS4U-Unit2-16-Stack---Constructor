b851917a7ffa5b78e0f492a1ca98bb1373e144f6
Ubuntu <ubuntu@ip-172-31-23-173.ec2.internal>
2020-11-07 22:23:05 +0000

    completed

--- StackInt.java
+++ StackInt.java
@@ -1,37 +1,70 @@
-import java.io;
-import java.util.Scanner;
+import java.util.*;
 /**
- * Class MyIntStack
- */
-/**
-*
+
+* Class MyIntStack
+
 * @author  Andrew Kuekam
 * @version 1.0
 * @since   2020-10-23 
 */
-public class StackIn {
-  
+public class StackInt {
+    /**
+    * .
+    */
     //private properties
-    private stack (< Interger > stack) {
-      
-      // constuctor
-      public stackInt () {
-        stat = new stack ( < Interger > );
-      }
-      
-      public void pushInt ( Interger ) {
-        stack.push;
-      }
-      
-      public void PopInt ( Interger ) {
-        stack.Pop;
-      }
-      
-      public void PeekInt ( Interger ) {
-        stack.Peek;
-      }
-      public void ClearInt ( Interger ) {
-        stack.Clear;
-      }
+    private Stack<Integer> stack;
+    /**
+    * .
+    */    
+    // constuctor
+    public StackInt() {
+      // This create an  empty stack of integers
+      stack = new Stack<Integer>();
     }
-  }
+    /**
+    * .
+    */
+    //create a public stacksize
+    public int Stacksize() {
+      // Creating an empty Stack 
+      int size = stack.size();
+      //this return the size
+      return size;
+    }
+    /**
+    * .
+    */
+    //This create a public push
+    public int pushInt(int number) {
+      //return the push number in the stack
+      return stack.push(number);
+    }
+    /**
+    * .
+    */
+    //This create the public Pop
+    public int PopInt() {
+      //user number to stackPop
+      int number = stack.pop();
+      //This return the number from the top of the stack
+      return number;
+    }
+    /**
+    * .
+    */
+    //This create the public Peek
+    public int PeekInt() {
+      //user number to the stackpeek
+      int number = stack.peek();
+      //return the number peeked
+      return number;
+    }
+    /**
+    * .
+    */
+    //this create the public clear
+    public int ClearInt() {
+      int number = Stack.clear();
+      return number;
+    }
+}
--- StackIntTest.java
+++ StackIntTest.java
@@ -1,31 +1,116 @@
+import java.util.Scanner;
+import java.util.*;
 /**
- * Class stackTest
- */
- /**
+* Class stackTest
+*/
+/**
 *
 * @author  Andrew Kuekam
 * @version 1.0
 * @since   2020-10-23 
 */
-public class stackTest {
+public class StackIntTest {
   public static void main(String[] args) {
     
     //creat the stack
-    stackInt mystack = new stack();
+    StackInt mystack = new StackInt();
     
-    //this get the interger from the user
-    for(int Interger = 0; Interger < 5; Interger++) {
-      
-    //push it onto the stack
-    mystack.pushInt(Interger);
-     
-    //Pop it onto the stack
-    mystack.PopInt(Interger);
-      
-    //Peek it onto the stack
-    mystack.PeekInt(Interger);
+    // let user input
+    Scanner userInput = new Scanner(System.in);
     
-    //clear it onto the stack
-    mystack.ClearInt(Interger);
+    //This print how it works
+    System.out.println("In a stack, all operation takes place at the TOP of the stack . ");
+    System.out.println("The PUSH operation adds an item to the of the stack. ");
+    System.out.println("The POP operation remove the item on the top of the stack and retuns it:");
+    System.out.println("");
+    
+    System.out.println("Enter 1 for PUSH:");
+    System.out.println("Enter 2 for POP:");
+    System.out.println("Enter 3 PEEK:");
+    System.out.println("Enter 4 CLEAR");
+    System.out.println("==============================");
+    System.out.println(" ");
+    
+    
+    // bool true
+    boolean running = true; 
+    while (running) {
+      
+      int number = userInput.nextInt();
+      
+      //push it onto the stack
+      if (number == 1 ) { 
+        
+        // ask number 
+        System.out.println(" Enter an Integer");
+        
+        int userPush = userInput.nextInt();
+        
+        //call method
+        mystack.pushInt(userPush);
+        
+        // printy
+        System.out.println(userPush + " has been pushed to stack");
+        System.out.println("==============================");
+        System.out.println(" ");
+        
+        // for Pop
+      } else if (number == 2) {
+        
+        //call size
+        int size = mystack.Stacksize();
+        if (size == 0)  {
+          //This prints the stack is emty.
+          System.out.println(" Error. Stack empty");
+          System.out.println("=============================");
+          System.out.println(" ");
+          
+        } else  {
+          //call stacksize method
+          int poppedInt = mystack.PopInt();
+          System.out.println(poppedInt + " is Poped");
+          System.out.println("=============================");
+          System.out.println(" ");
+          
+          }
+      } else if (number == 3) {
+          int PeekingInt = mystack.PeekInt();
+          System.out.println(PeekingInt + " is at the top of the stark");
+          System.out.println("=============================");
+          System.out.println(" ");
+          
+          //
+          int size = mystack.Stacksize();
+          if (size == 0) {
+            //This prints the stack is emty.
+            System.out.println(" Error. Stack empty");
+            System.out.println("=============================");
+            System.out.println(" ");
+          }
+          
+      } else if (number == 4) {
+        //
+        int ClearingInt = mystack.ClearInt();
+        System.out.println(ClearingInt + " is Clear from the stark");
+        System.out.println("=============================");
+        System.out.println(" ");
+        
+        //
+        int size = mystack.Stacksize();
+        if (size == 0) {
+          //This prints the stack is emty.
+          System.out.println(" Error. Stack empty");
+          System.out.println("=============================");
+          System.out.println(" ");
+        }
+          
+      } else { 
+        System.out.println("Enter a valid number");
+        System.out.println("1 to PUSH:");
+        System.out.println("2 to for POP:");
+        System.out.println("3 to PEEK:");
+        System.out.println("4 to CLEAR");
+      }
+    }
   }
 }
